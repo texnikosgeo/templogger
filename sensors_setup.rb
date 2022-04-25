24be00d74beb70d415d8c04808b0ce2f5804bec0
texnikosgeo <103029638+texnikosgeo@users.noreply.github.com>
2022-04-25 12:15:35 +0300

    Create sensors_setup.rb

---
+++ sensors_setup.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+puts " Setup sensors names e.x. 28-01144ee*****/ -> Freezer 01
+        creates a folder in home directory with name given and
+        assosiates sensor with it
+     "
+puts `ls /sys/bus/w1/devices/`
+puts "sensor setup is:"
+puts `cat /home/alerter/templogger/sensors/sensors`
+puts "whitch sensor you want to setup ?
+give full name."
+sensor_name = gets.chomp
+puts "Give desired name:"
+desired_name = gets.chomp
+# puts `mkdir -p /home/alerter/templogger/sensors`
+puts `mkdir -p /home/alerter/templogger/sensors/`
+puts `mkdir -p ~/templogger/logs/"#{desired_name}"`
+puts `touch /home/alerter/templogger/sensors/sensors`
+puts `touch /home/alerter/templogger/logs/"#{desired_name}"/log.txt`
+puts `ln -s /sys/bus/w1/devices/"#{sensor_name}"/w1_slave ~/templogger/sensors/"#{desired_name}"`
+puts "#{desired_name} is in home/alerter/templogger/sensors"
+puts `echo "#{sensor_name}, #{desired_name}" >> /home/alerter/templogger/sensors/sensors`
+
+#puts `echo "#{desired_name}" = Ds18b20::Parser.new\(\"/sys/bus/w1/devices/"#{sensor_name}"/w1_slave\"\) >> ~/templogger/temp_real_time.rb`
+#puts `echo puts "temp for #{desired_name} is #{desired_name}.celsius celsius"  >> ~/templogger/temp_real_time.rb`
